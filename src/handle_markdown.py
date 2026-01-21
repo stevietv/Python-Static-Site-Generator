0c831a1b9eb8013b9f1cea522d3396a221b33063
StevieTV <stevie.robinson@gmail.com>
2026-01-21 17:29:51 +0100

    handle links and images

--- src/handle_markdown.py
+++ src/handle_markdown.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -17,3 +18,14 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
         new_nodes.extend(new_node)
     return new_nodes
+
+def extract_markdown_images(text):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
--- src/test_handle_markdown.py
+++ src/test_handle_markdown.py
@@ -1,8 +1,8 @@
 import unittest
-from handle_markdown import split_nodes_delimiter
+from handle_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
-class TestHTMLNode(unittest.TestCase):
+class TestHandleMarkdownNode(unittest.TestCase):
     def test_code(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
@@ -52,6 +52,24 @@ class TestHTMLNode(unittest.TestCase):
         newer_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
 
         self.assertEqual(len(newer_nodes), 5)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://blog.boot.dev"),
+            ],
+            matches,
+        )
 
 if __name__ == "__main__":
     unittest.main()
