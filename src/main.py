814ba94c5d6b97684b577c54a2759143a504a522
StevieTV <stevie.robinson@gmail.com>
2026-01-23 20:15:46 +0100

    ensure static is also copied to docs

--- src/main.py
+++ src/main.py
@@ -4,26 +4,26 @@ import sys
 from generate_static import generate_pages_recursive, generate_page
 
 def main():
-    basepath = sys.argv[0]
+    basepath = sys.argv[1]
     publish_static_to_public()
     generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
 def publish_static_to_public():
-    if os.path.exists("public"):
-        shutil.rmtree("public")
+    if os.path.exists("docs"):
+        shutil.rmtree("docs")
     
     files_to_copy = os.listdir("static")
     copy_files(files_to_copy)
 
 def copy_files(file_list, base_path = ""):
-    dest_folder = os.path.join("public", base_path)
+    dest_folder = os.path.join("docs", base_path)
     if not os.path.exists(dest_folder):
         os.mkdir(dest_folder)
     for item in file_list:
         source = os.path.join("static", base_path, item)
         if os.path.isfile(source):
-            dest = os.path.join("public", base_path)
+            dest = os.path.join("docs", base_path)
             print(f"copying file from {source} to {dest}")
             shutil.copy(source, dest)
         else:
